4dac8b9bd692d7293472cb65a5cd80d0c8bb8378
SimoVisc <simonaviscardivs@gmail.com>
2023-02-02 14:42:41 +0100

    Definisce le rotte

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Rotte pubbliche del frontoffice
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// Tutte le rotte protette da autenticazione del backoffice
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
 });
 
+// Tutte le rotte di autenticazione (registrazione, login ecc...)
 require __DIR__.'/auth.php';
